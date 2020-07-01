0087f74801e8f815c85d5190ea271a4d5ac59c91
Ashutosh Sanzgiri <sanzgiri@gmail.com>
2020-07-01 16:50:16 -0700

    fix cv2 issue

--- app.py
+++ app.py
@@ -143,9 +143,9 @@ def cartoonize(infile, outfile, model_path):
     sess.run(tf.global_variables_initializer())
     saver.restore(sess, tf.train.latest_checkpoint(model_path))
 
-    image = cv2.imread(infile)
-    #image = infile
-    #image = resize_crop(image)
+    #image = cv2.imread(infile)
+    image = infile
+    image = resize_crop(image)
     batch_image = image.astype(np.float32) / 127.5 - 1
     batch_image = np.expand_dims(batch_image, axis=0)
     output = sess.run(final_out, feed_dict={input_photo: batch_image})
